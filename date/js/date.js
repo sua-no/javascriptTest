ac85efe18df693f4c5b5abe2bf3ff6ec379c6760
sua-no <sua953@naver.com>
2020-02-18 17:50:23 +0900

    date완성

--- date/js/date.js
+++ date/js/date.js
@@ -1,12 +1,8 @@
-var date = new Date();
-var year = date.getFullYear();
-var month = date.getMonth() + 1;
-var day = date.getDay();
-var hours = date.getHours();
-var minutes = date.getMinutes();
-var seconds = date.getSeconds();
 target = document.getElementById("time");
-function time() {
-    target.innerHTML += date;
-}
-time();
+
+setInterval(function() {
+    setInterval(function() {
+        var now = new Date();
+        target.innerHTML = now.toString();
+    }, 1000);
+});
